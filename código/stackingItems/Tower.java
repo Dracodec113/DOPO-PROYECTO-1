2fef346c3c617483de73048452301c4c21acf9a6
juanbueno1980 <95256961+juanbueno1980@users.noreply.github.com>
2026-02-28 17:29:08 -0500

    Se realizan nuevos metodos, order y reverse

--- código/stackingItems/Tower.java
+++ código/stackingItems/Tower.java
@@ -13,9 +13,9 @@ public class Tower
 {
     private int height;
     private int width;
-    private HashMap<Integer, Cup> cups;
-    private HashMap<Integer, Lid> lids;
-
+    private HashMap<Integer, StackingItem> items;
+    private ArrayList<Integer> order;
+    
     public Tower(int height, int width){
         this.height = height;
         this.width = width;
@@ -26,35 +26,84 @@ public class Tower
     public void pushCup(int id){
         if(!checkCup(id)){
             Cup cup = new Cup();
-            cups.put(id, cup);
+            items.put(id, cup);
+            order.add(id);
         }
     }
     
     public void pushLid(int id){
          if(!checkLid(id)){
             Lid lid = new Lid();
-            lids.put(id, lid);
+            items.put(-id, lid);
+            order.add(-id);
         }
     }
     
     public void removeCup(int id){
         if(checkCup(id)){
-            cups.remove(id);
+            items.remove(id);
+            order.remove(Integer.valueOf(id));
         }
     }
+    
     public void removeLid(int id){
         if(checkLid(id)){
-            lids.remove(id);
+            items.remove(id);
+            order.remove(Integer.valueOf(-id));
         }
     }
+    
     public void orderTower(){
+        ArrayList<Integer> posicions = new ArrayList<>();
         
+        for(Integer id:items.keySet()){
+            
+            int cup=Math.abs(id);//Agrega primero tasas si existe.
+            if (!posicions.contains(cup)){
+                    posicions.add(cup);
+            }
+            Collections.sort(posicions, Collections.reverseOrder());
+        }
+        order.clear();
+        for(Integer pos:posicions) {
+            if (items.containsKey(pos)) {
+                order.add(pos);
+            }
+
+            if (items.containsKey(-pos)) {
+                order.add(-pos);
+            }
+        }
     }
+    
+        public void reverseTower(){
+        ArrayList<Integer> posicions = new ArrayList<>();
+        
+        for(Integer id:items.keySet()){
+            
+            int cup=Math.abs(id);//Agrega primero tasas si existe.
+            if (!posicions.contains(cup)){
+                    posicions.add(cup);
+            }
+            Collections.sort(posicions);
+        }
+        order.clear();
+        for(Integer pos:posicions) {
+            if (items.containsKey(pos)) {
+                order.add(pos);
+            }
+
+            if (items.containsKey(-pos)) {
+                order.add(-pos);
+            }
+        }
+    }
+    
     private boolean checkCup(int id){
-        return cups.containsKey(id);
+        return items.containsKey(id);
     }
     
     private boolean checkLid(int id){
-        return cups.containsKey(id);
+        return items.containsKey(-id);
     }
 }
