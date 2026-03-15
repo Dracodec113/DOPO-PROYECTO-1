12ec0d9659896ed871e8a17bd304d5c50255ef09
juanbueno5555-rgb <juan.rangel-j@mail.escuelaing.edu.co>
2026-03-14 23:18:15 -0500

    logica agregada,
    ya acabe lo del ultimo ciclo:v

--- código/stackingItems/TowerContest.java
+++ código/stackingItems/TowerContest.java
@@ -1,96 +1,185 @@
 /**
+
  * Resuelve y simula el Problem J - Stacking Cups.
+
  *
+
  * @author Jeronimo Moreno
+
  * @version 1.0
+
  */
+
 public class TowerContest {
 
+
+
     public static String solve(int n, int h) {
-        int[] heights = new int[n];
+
+        int[] cupsHeights= new int[n];
+
         for (int i = 0; i < n; i++) {
-            heights[i] = 2 * (i + 1) - 1;
+
+            cupsHeights[i]=2*(i+1)-1;
+
         }
-        String result = permute(heights, 0, h);
+
+        String result = permute(cupsHeights, h,0);
+
         if (result == null) return "impossible";
+
         return result;
+
     }
+
+
 
     public static void simulate(int n, int h) {
+
         String result = solve(n, h);
+
         if ("impossible".equals(result)) {
+
             javax.swing.JOptionPane.showMessageDialog(
+
                 null, "Imposible: no existe orden para altura " + h);
+
             return;
+
         }
-        Tower tower = new Tower(25, 25);
-        for (String s : result.split(" ")) {
+
+        Tower tower=new Tower(25, 25);
+
+        for (String s:result.split(" ")) {
+
             int height = Integer.parseInt(s);
-            int id = (height + 1) / 2;
+
+            int id=(height+1)/2;
+
             tower.pushCup(id);
+
         }
+
     }
 
-    private static String permute(int[] arr, int start, int h) {
-        if (start == arr.length) {
-            int altura = calculateHeight(arr);
-            System.out.println(arrayToString(arr) + " -> " + altura);
-            if (altura == h) return arrayToString(arr);
+
+
+
+
+    private static String permute(int[] cupsHeights, int h, int index) {
+
+        if (index == cupsHeights.length) {
+
+            if (calculateHeight(cupsHeights) == h) {
+
+                return arrayToString(cupsHeights);
+
+            }
+
             return null;
+
         }
-        for (int i = start; i < arr.length; i++) {
-            swap(arr, start, i);
-            String result = permute(arr, start + 1, h);
-            if (result != null) return result;
-            swap(arr, start, i);
+
+    
+
+        for (int i = index; i < cupsHeights.length; i++) {
+
+            swap(cupsHeights, index, i);
+
+            String res = permute(cupsHeights, h, index + 1);
+
+            if (res != null) return res; 
+
+            swap(cupsHeights, index, i); 
+
         }
+
         return null;
+
     }
+
+
 
     private static int calculateHeight(int[] order) {
-        int towerHeight = 0;
-        int floorInside = 0;
-        int container = 0;
 
-        System.out.print("  calculando: ");
-        for (int i = 0; i < order.length; i++) {
-            int cup = order[i];
-            System.out.print("cup=" + cup + " tower=" + towerHeight + 
-                           " container=" + container + " floor=" + floorInside + " | ");
-            
-            if (towerHeight == 0) {
-                towerHeight = cup;
-                container = cup;
-            } else if (cup < container) {
-                if (cup + floorInside > container) {
-                    towerHeight = towerHeight - container + cup + floorInside;
-                    container = cup + floorInside + 1;
-                    floorInside = 0;
-                } else {
-                    floorInside = Math.max(floorInside, cup + 1);
+        if (order.length == 0) return 0;
+
+    
+
+        int totalHeight = order[0];
+
+        int currentContainer = order[0];
+
+        int maxInsideHeight = 0;
+
+    
+
+        for (int i = 1; i < order.length; i++) {
+
+            int nextCup = order[i];
+
+            if (nextCup < currentContainer) {
+
+                if (nextCup > maxInsideHeight) {
+
+                    maxInsideHeight = nextCup;
+
                 }
+
+                if (1 + maxInsideHeight > currentContainer) {
+
+                    int excess = (1 + maxInsideHeight) - currentContainer;
+
+                    totalHeight += excess;
+
+                    currentContainer += excess;
+
+                }
+
             } else {
-                towerHeight = towerHeight + cup;
-                container = cup;
-                floorInside = 0;
+
+                totalHeight += nextCup;
+
+                currentContainer = nextCup;
+
+                maxInsideHeight = 0; 
+
             }
+
         }
-        System.out.println("=> " + towerHeight);
-        return towerHeight;
+
+        return totalHeight;
+
     }
+
+    
 
     private static void swap(int[] arr, int i, int j) {
+
         int tmp = arr[i];
+
         arr[i] = arr[j];
+
         arr[j] = tmp;
+
     }
 
+
+
     private static String arrayToString(int[] arr) {
-        String result = "";
+
+        String result="";
+
         for (int i = 0; i < arr.length; i++) {
-            if (i > 0) result += " ";
+
+            if (i>0) result+=" ";
+
             result += arr[i];
+
         }
+
         return result;
+
     }
+
 }
